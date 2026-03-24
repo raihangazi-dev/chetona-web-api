71ae79c0dcc1d7b4b6373eef1d5985f7e9592e90
raihangazi-dev <abul.khayer.raihan@gmail.com>
2026-03-24 17:02:11 +0600

    Static CRUD Endpoints created

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -16,14 +18,71 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 // Basic CRUD API endpoints Creation
+List<Category> Categories = new List<Category>();
+
 app.MapGet("", () =>
 {
     return Results.Ok("Welcome to Chetona Prokashon Web Server");
 });
 
-app.MapGet("/api/v1/category", () =>{});
-app.MapPost("/api/v1/category",() => {});
-app.MapPut("/api/v1/category", () => {});
-app.MapDelete("/api/v1/category", () => {});
+app.MapGet("/api/v1/category", () =>
+{
+    // var response = new {status = "Success", message = "Respnse for get all category"};
+    // return response;
+
+    return Results.Ok(Categories);
+
+});
+
+app.MapPost("/api/v1/category", () =>
+{
+    var newCategory = new Category
+    {
+        Id = Guid.NewGuid(),
+        Name = "History",
+        Description = "Know about the history",
+        CreatedAt = DateTime.UtcNow
+    };
+
+    Categories.Add(newCategory);
+
+    return Results.Created($"/api/v1/category/{newCategory.Id}", newCategory);
+});
+
+app.MapPut("/api/v1/category", () =>
+{
+    var foundedCategory = Categories.FirstOrDefault(Category => Category.Id == Guid.Parse("693dfed0-4d03-49f8-8f63-2385b679f27f"));
+    if(foundedCategory == null)
+    {
+        return Results.NotFound("Category with this Id does not Exist");
+    }
+
+    foundedCategory.Name = "Updated Name";
+    foundedCategory.Description = "Updated Description";
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/api/v1/category", () =>
+{
+    var foundedCategory = Categories.FirstOrDefault(Category => Category.Id == Guid.Parse("693dfed0-4d03-49f8-8f63-2385b679f27f"));
+
+    if(foundedCategory == null)
+    {
+        return Results.NotFound("Category with this Id does not exist");
+    }
+
+    Categories.Remove(foundedCategory);
+
+    return Results.NoContent();
+});
 
 app.Run();
+
+public record Category
+{
+    public Guid Id {get; set;}
+    public string? Name {get; set;}
+    public string? Description {get; set;}
+    public DateTime CreatedAt {get; set;}
+}
