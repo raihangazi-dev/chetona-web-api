6aeaf2e42d7b40c15f07714f0949b6daeeb991c9
raihangazi-dev <abul.khayer.raihan@gmail.com>
2026-03-24 15:55:53 +0600

    Swagger added

--- Program.cs
+++ Program.cs
@@ -1,18 +1,29 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+// Basic CRUD API endpoints Creation
+app.MapGet("", () =>
+{
+    return Results.Ok("Welcome to Chetona Prokashon Web Server");
+});
 
+app.MapGet("/api/v1/category", () =>{});
+app.MapPost("/api/v1/category",() => {});
+app.MapPut("/api/v1/category", () => {});
+app.MapDelete("/api/v1/category", () => {});
 
 app.Run();
-
